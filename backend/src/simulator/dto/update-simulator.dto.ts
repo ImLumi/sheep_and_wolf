aa83e08f1f17fcec2227d5aeca834256979143ec
bencesimon <mikesimon150@gmail.com>
2022-09-14 01:22:36 +0200

    rename

--- backend/src/simulator/dto/update-simulator.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSimulatorDto } from './create-simulator.dto';
-
-export class UpdateSimulatorDto extends PartialType(CreateSimulatorDto) {
-  id: number;
-}
