c4a33c63aea2394c00169c5ad017dac715ddbf4e
bencesimon <mikesimon150@gmail.com>
2022-09-14 12:11:51 +0200

    feat(): add environment variable

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SimulationModule } from './simulation/simulation.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [SimulationModule],
+  imports: [
+    SimulationModule,
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
